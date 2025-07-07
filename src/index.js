a0bcaa61f7fc2aafb7f9838314f88a70cc4d54e4
Dmitro-SixSanSor <dmytro.ostaphiychuk@gmail.com>
2025-07-07 20:38:49 +0300

    add markup header

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import { config } from 'dotenv';
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
-
-config(); 
 
 const bootstrap = async () => {
   await initMongoConnection();
